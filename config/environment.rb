5b1bad9cef37bb98615a214ab484ae0a79ea7338
Damon Tran <damontran_@hotmail.com>
2017-03-14 00:06:24 +0000

    Added mailer settings to enviroment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActiveMailer::Base.smtp_settings = {
+    :port           => ENV[MAILGUN_SMPT_PORT],
+    :address        => ENV[MAILGUN_SMPT_SERVER],
+    :username       => ENV[MAILGUN_SMPT_LOGIN],
+    :password       => ENV[MAILGUN_SMPT_PASSWORD],
+    :domain         => 'stormy-depths-50973.herokuapp.com',
+    :authentication => :plain,
+}
+
+ActionMailer::Base.delivery_method = :smpt
