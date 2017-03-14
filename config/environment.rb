5d940a8d6414a91f9949acae8ff40500ceb2b6b9
Damon Tran <damontran_@hotmail.com>
2017-03-14 00:19:46 +0000

    rolling back mailgun changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-    :port           => ENV[MAILGUN_SMPT_PORT],
-    :address        => ENV[MAILGUN_SMPT_SERVER],
-    :user_name      => ENV[MAILGUN_SMPT_LOGIN],
-    :password       => ENV[MAILGUN_SMPT_PASSWORD],
-    :domain         => 'stormy-depths-50973.herokuapp.com',
-    :authentication => :plain,
-}
-
-ActionMailer::Base.delivery_method = :smpt
