7f734cac3f461927f3a9ff84d2b49c7b85f96c83
Damon Tran <damontran_@hotmail.com>
2017-04-07 20:39:16 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # Renger a blank details form
+    @profile = Profile.new
   end
   
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,3 @@
-class Profile < ActionRecord::Base
+class Profile < ActiveRecord::Base
   belongs_to :user
 end
